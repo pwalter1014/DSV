269cc244e17838b743b0de3eb130251ddd3f2bdb
vickEretus <vpineda@ycp.edu>
2022-11-05 18:36:01 -0400

    Improved readability

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, SafeAreaView, Alert } from "react-native";
+import { StyleSheet, BackHandler, Text, View, Button, SafeAreaView, Alert } from "react-native";
 import Search from './Search';
 import { getInspectorDataForInstance } from "react-native/Libraries/Renderer/implementations/ReactNativeRenderer-dev";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 function HomeScreen({navigation}){
   return (
@@ -14,30 +15,31 @@ function HomeScreen({navigation}){
 <Text style={styles.header}>Welcome, {"\n"}Data Structure Deadheads</Text>
 <Text style={styles.middleText}>Please select an Option Below</Text>
 
-<<<<<<< Updated upstream
-=======
   <Button style={styles.buttons}
     
     title="Instructions"
     color="#3a8727"
     onPress={() => navigation.navigate('Instructions', {name: 'Instructions'})} />
->>>>>>> Stashed changes
 
-      <Button style={styles.buttons}
-        title="Arrays"
-        color="#3a8727"
-        onPress={() => navigation.navigate('Array', {name: 'Array'})} />
+  <Button style={styles.buttons}
+    title="Arrays"
+    color="#3a8727"
+    onPress={() => navigation.navigate('Array', {name: 'Array'})} />
 
-      <Button style={styles.buttons}
-        title="Linked Lists"
-        color="#3a8727"
-        onPress={() => Alert.alert('Simple Button pressed')} />
+  <Button style={styles.buttons}
+    title="Linked Lists"
+    color="#3a8727"
+    onPress={() => navigation.navigate('Linked List', {name: 'Linked List'})} />
 
- <Button style={styles.buttons}
-  title="Trees"
-  color="#3a8727"
-  onPress={() => Alert.alert('Simple Button pressed')} />
+  <Button style={styles.buttons}
+    title="Binary Search Trees"
+    color="#3a8727"
+    onPress={() => navigation.navigate('Binary Search Tree', {name: 'Binary Search Tree'})} />
 
+  <Button style={styles.buttons}
+    title="Quit"
+    color="#3a8727"
+    onPress={() => BackHandler.exitApp()} />
 
 <StatusBar style="auto" />
     </View>
@@ -54,6 +56,34 @@ function Array() {
   );
 };
 
+function LinkedList() {
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.header}>Array, {"\n"}Arrays</Text>
+      <Text style={styles.middleText}>Please select an Option Below</Text>
+      </View>
+  );
+};
+
+function Bst() {
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.header}>Array, {"\n"}Arrays</Text>
+      <Text style={styles.middleText}>Please select an Option Below</Text>
+      </View>
+  );
+};
+
+function Instructions() {
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.header}>Instructions Placeholder</Text>
+    </View>
+  )
+}
 
 const Stack = createNativeStackNavigator();
 
@@ -64,12 +94,27 @@ export default function App() {
           <Stack.Screen
             name="Home"
             component={HomeScreen}
-            options={{title: 'Welcome' }}
+            options={{title: 'Welcome'}}
           />
 
           <Stack.Screen 
             name="Array"
             component={Array}
+          />
+
+          <Stack.Screen 
+            name="Linked List"
+            component={LinkedList}
+          />
+
+          <Stack.Screen 
+            name="Binary Search Tree"
+            component={Bst}
+          />
+
+          <Stack.Screen 
+            name="Instructions"
+            component={Instructions}
           />
 
         </Stack.Navigator>
