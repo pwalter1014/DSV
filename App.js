a559a313d1b49eb00564e4e1b1628d4f3d79f24f
Gorgon954 <Mwilliams28@ycp.edu>
2022-11-05 02:49:27 -0400

    Understanding React and Adding buttons

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView, Alert } from 'react-native';
+import Search from './Search';
+
+import React, { Component } from 'react'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       
+      <Text style={styles.header}>Welcome, {"\n"}Data Structure Deadheads</Text>
+      <Text style={styles.middleText}>Please select an Option Below</Text>
+      
+     
+     
+      <Button style={styles.buttons}
+        title="Geeks"
+        color="#3a8727"
+        onPress={() => Alert.alert('Simple Button pressed')}/>
+       
+        
+          <StatusBar style="auto" />
     </View>
   );
 }
@@ -13,8 +27,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#37353d',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  header: {
+    color: '#3a8727',
+    width: 400,
+    fontSize: 15,
+    textAlign: 'left',
+    marginTop: 40,
+    marginLeft: 15,
+  },
+
+  middleText: {
+    flex: 6,
+    color: '#3a8727',
+    textAlign: 'center',
+    fontSize: 25,
+    marginTop: 75,
+    width: 400,
+  },
+
+  buttons: {
+    boder: 'none',
+    color: 'pink',
+  }
 });
