958bf5a4ad583df5857782ed8e97f1c3d37e17f3
vickEretus <vpineda@ycp.edu>
2022-11-05 18:34:16 -0400

    Improved readability

--- App.js
+++ App.js
@@ -9,10 +9,19 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 function HomeScreen({navigation}){
   return (
 <View style={styles.container}>
+  
 
 <Text style={styles.header}>Welcome, {"\n"}Data Structure Deadheads</Text>
 <Text style={styles.middleText}>Please select an Option Below</Text>
 
+<<<<<<< Updated upstream
+=======
+  <Button style={styles.buttons}
+    
+    title="Instructions"
+    color="#3a8727"
+    onPress={() => navigation.navigate('Instructions', {name: 'Instructions'})} />
+>>>>>>> Stashed changes
 
       <Button style={styles.buttons}
         title="Arrays"
@@ -70,12 +79,21 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  layout: {
     flex: 1,
-    backgroundColor: '#37353d',
-    alignItems: 'center',
+    backgroundColor: '#e5e5e5',
     justifyContent: 'center',
-
+  },
+  card: {
+    width: 100, 
+    height: 100, 
+    backgroundColor: 'white', 
+    margin: 16, 
+    borderRadius: 2, 
+    shadowColor: 'black', 
+    shadowOpacity: 0.3, 
+    shadowRadius: 1, 
+    shadowOffset: { height: 1, width: 0.3 } 
   },
 
   header: {
